48081d1defb1b3594c8e90c4a3d809a84b531351
obnosim <obnosim@gmail.com>
2015-12-03 10:38:56 -0500

    Ajouts des fonctions de la vue de gestion des tâches

--- InfoDynamiqueQt/main.cpp
+++ InfoDynamiqueQt/main.cpp
@@ -1,10 +1,17 @@
 #include "Controleurs/controleurapplication.h"
 #include <QApplication>
+#include <Vues/vuegestionpiece.h>
 
 int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
-    ControleurApplication controleurApplication;
+//    ControleurApplication controleurApplication;
+    VueGestionPiece* vue = new VueGestionPiece();
+    vue->setWindowTitle("slut");
+    vue->setNom((QString) "banana");
+    vue->setPrix(245.02);
+    vue->setDescription((QString) "Soon you will all pay");
+    vue->show();
 
     return a.exec();
 }
