9052a4548c7ed9f0d5a18d1778dfa4e2a6f04086
obnosim <obnosim@gmail.com>
2015-12-09 09:57:10 -0500

    Documentation; revue des requêtes SQL

--- InfoDynamiqueQt/Controleurs/controleurclients.cpp
+++ InfoDynamiqueQt/Controleurs/controleurclients.cpp
@@ -5,20 +5,32 @@
 #include <QSqlQueryModel>
 #include <QDebug>
 
-ControleurClients::ControleurClients(VuePrincipale *vuePrincipale, QObject *parent) : QObject(parent)
-{
+ControleurClients::ControleurClients(VuePrincipale* vuePrincipale, QObject* parent)
+    : QObject(parent) {
+    vueSecondaireClients = new VueSecondaire();
+    vuePrincipale->getUi()->ongletClients->layout()->addWidget(vueSecondaireClients);
     controleurGestionClient = new ControleurGestionClient();
     controleurGestionAppareil = new ControleurGestionAppareil();
     controleurGestionFiche = new ControleurGestionFiche();
-    vueSecondaireClients = new VueSecondaire();
-
     configurerFragmentClients();
     configurerFragmentAppareils();
     configurerFragmentFiches();
 
-    peuplerClients();
+    requeteClients = new QString(
+                "select\
+                      c.id as '#',\
+                      coalesce(a.nb,0) as 'Nb. appareils',\
+                      c.nom as 'Nom',\
+                      c.telephone as 'Téléphone',\
+                      c.adresse as 'Adresse'\
+                  from\
+                      clients c\
+                    left outer join\
+                      (select idClient, count(id) as 'nb' from appareils group by idClient) a\
+                    on\
+                      c.id = a.idClient");
 
-    vuePrincipale->getUi()->ongletClients->layout()->addWidget(vueSecondaireClients);
+    peuplerClients();
 }
 
 void ControleurClients::configurerFragmentClients() {
@@ -26,27 +38,14 @@ void ControleurClients::configurerFragmentClients() {
     vueSecondaireClients->getUi()->verticalLayout->addWidget(fragmentClients);
     fragmentClients->getEtiquette()->setText("Clients");
     QPushButton* boutonAjouter = fragmentClients->getBouton1();
-    boutonAjouter->setText("Ajouter");
     boutonAjouter->setIcon(QIcon::fromTheme("list-add-user"));
     QPushButton* boutonModifier = fragmentClients->getBouton2();
-    boutonModifier->setText("Modifier");
     boutonModifier->setIcon(QIcon::fromTheme("document-edit"));
-    boutonModifier->setDisabled(true);
     QPushButton* boutonVisualiser = fragmentClients->getBouton3();
-    boutonVisualiser->setText("Visualiser");
     boutonVisualiser->setIcon(QIcon::fromTheme("user-properties"));
-    boutonVisualiser->setDisabled(true);
     fragmentClients->getCaseCocher()->hide();
     QObject::connect(fragmentClients->getBouton1(), SIGNAL(pressed()), controleurGestionClient, SLOT(ajouterClient()));
-}
-
-void ControleurClients::peuplerClients()
-{
-    QSqlQueryModel* clients = new QSqlQueryModel(this);
-    const QSqlDatabase bd = QSqlDatabase::database("dossiers");
-    const QString* requete = new QString("SELECT * FROM clients");
-    clients->setQuery(*requete, bd);
-    fragmentClients->setModele(clients);
+    QObject::connect(fragmentClients->getTableau(), SIGNAL(activated(QModelIndex)), this, SLOT(selectionnerClient(QModelIndex)));
 }
 
 void ControleurClients::configurerFragmentAppareils() {
@@ -54,39 +53,18 @@ void ControleurClients::configurerFragmentAppareils() {
     vueSecondaireClients->getUi()->verticalLayout->addWidget(fragmentAppareils);
     fragmentAppareils->getEtiquette()->setText("Appareils");
     QPushButton* boutonAjouter = fragmentAppareils->getBouton1();
-    boutonAjouter->setText("Ajouter");
     boutonAjouter->setIcon(QIcon::fromTheme("list-add"));
     QPushButton* boutonModifier = fragmentAppareils->getBouton2();
-    boutonModifier->setText("Modifier");
-    boutonModifier->setDisabled(true);
     boutonModifier->setIcon(QIcon::fromTheme("document-edit"));
     QPushButton* boutonVisualiser = fragmentAppareils->getBouton3();
-    boutonVisualiser->setText("Visualiser");
-    boutonVisualiser->setDisabled(true);
+    boutonVisualiser->setIcon(QIcon::fromTheme("document-preview"));
     fragmentAppareils->getCaseCocher()->hide();
-    fragmentAppareils->setHidden(true);
-    QObject::connect(fragmentAppareils->getBouton1(), SIGNAL(pressed()), controleurGestionAppareil, SLOT(ajouterAppareil()));
-    QObject::connect(fragmentClients->getTableau(), SIGNAL(clicked(QModelIndex)), this, SLOT(clientSelectionne(QModelIndex)));
-}
-
-void ControleurClients::clientSelectionne(QModelIndex index)
-{
-    int colonne = 0;
-    int rangee = index.row();
-    QModelIndex index2 = fragmentClients->getTableau()->model()->index(rangee, colonne);
-    QVariant id = fragmentClients->getTableau()->model()->data(index2);
-    peuplerAppareils(id.toInt());
-    fragmentAppareils->show();
-    fragmentFiches->hide();
-}
-
-void ControleurClients::peuplerAppareils(int idClient)
-{
-    QSqlQueryModel* appareils = new QSqlQueryModel(this);
-    const QSqlDatabase bd = QSqlDatabase::database("dossiers");
-    QString requete = "SELECT * FROM appareils WHERE idClient="+QString::number(idClient);
-    appareils->setQuery(requete, bd);
-    fragmentAppareils->setModele(appareils);
+    QObject::connect(boutonAjouter, SIGNAL(pressed()), controleurGestionAppareil, SLOT(ajouterAppareil()));
+    QObject::connect(this, SIGNAL(clientSelectionne(int)), this, SLOT(peuplerAppareils(int)));
+    QObject::connect(this, SIGNAL(clientSelectionne(int)), fragmentAppareils, SLOT(show()));
+    QObject::connect(this, SIGNAL(clientRelache()), this, SLOT(relacherAppareil()));
+    QObject::connect(this, SIGNAL(clientRelache()), fragmentAppareils, SLOT(hide()));
+    QObject::connect(fragmentAppareils->getTableau(), SIGNAL(pressed(QModelIndex)), this, SLOT(selectionnerAppareil(QModelIndex)));
 }
 
 void ControleurClients::configurerFragmentFiches() {
@@ -94,38 +72,102 @@ void ControleurClients::configurerFragmentFiches() {
     vueSecondaireClients->getUi()->verticalLayout->addWidget(fragmentFiches);
     fragmentFiches->getEtiquette()->setText("Fiches");
     QPushButton* boutonAjouter = fragmentFiches->getBouton1();
-    boutonAjouter->setText("Ajouter");
     boutonAjouter->setIcon(QIcon::fromTheme("document-new"));
     QPushButton* boutonModifier = fragmentFiches->getBouton2();
-    boutonModifier->setText("Modifier");
-    boutonModifier->setDisabled(true);
     boutonModifier->setIcon(QIcon::fromTheme("document-edit"));
     QPushButton* boutonVisualiser = fragmentFiches->getBouton3();
-    boutonVisualiser->setText("Visualiser");
-    boutonVisualiser->setDisabled(true);
     boutonVisualiser->setIcon(QIcon::fromTheme("document-preview"));
     fragmentFiches->getCaseCocher()->setText("Afficher toutes les fiches");
-    fragmentFiches->hide();
-    QObject::connect(fragmentFiches->getBouton1(), SIGNAL(pressed()), controleurGestionFiche, SLOT(ajouterFiche()));
-    QObject::connect(fragmentAppareils->getTableau(), SIGNAL(clicked(QModelIndex)), this, SLOT(afficherFiches(QModelIndex)));
+    QObject::connect(boutonAjouter, SIGNAL(pressed()), controleurGestionFiche, SLOT(ajouterFiche()));
+    QObject::connect(this, SIGNAL(appareilSelectionne(int)), this, SLOT(peuplerFiches(int)));
+    QObject::connect(this, SIGNAL(appareilSelectionne(int)), fragmentFiches, SLOT(show()));
+    QObject::connect(this, SIGNAL(appareilRelache()), this, SLOT(relacherFiche()));
+    QObject::connect(this, SIGNAL(appareilRelache()), fragmentFiches, SLOT(hide()));
+    QObject::connect(fragmentFiches->getTableau(), SIGNAL(pressed(QModelIndex)), this, SLOT(selectionnerFiche(QModelIndex)));
 }
 
-void ControleurClients::peuplerFiches(int idAppareil)
-{
+void ControleurClients::peuplerClients() {
+    QSqlQueryModel* clients = new QSqlQueryModel(this);
+    const QSqlDatabase bd = QSqlDatabase::database("dossiers");
+    clients->setQuery(*requeteClients, bd);
+    fragmentClients->setModele(clients);
+    fragmentClients->getTableau()->resizeColumnsToContents();
+    relacherClient();
+}
+
+void ControleurClients::peuplerAppareils(int idClient) {
+    QSqlQueryModel* appareils = new QSqlQueryModel(this);
+    const QSqlDatabase bd = QSqlDatabase::database("dossiers");
+    appareils->setQuery(*requeteAppareils(idClient), bd);
+    fragmentAppareils->setModele(appareils);
+    fragmentAppareils->getTableau()->resizeColumnsToContents();
+    relacherAppareil();
+}
+
+QString *ControleurClients::requeteAppareils(int idClient) const {
+    return new QString("SELECT * FROM appareils WHERE idClient="+QString::number(idClient));
+}
+
+void ControleurClients::peuplerFiches(int idAppareil) {
     QSqlQueryModel* fiches = new QSqlQueryModel(this);
     const QSqlDatabase bd = QSqlDatabase::database("dossiers");
-    QString requete = "SELECT * FROM fiches WHERE idAppareil="+QString::number(idAppareil);
-    fiches->setQuery(requete, bd);
+    fiches->setQuery(*requeteFiches(idAppareil), bd);
     fragmentFiches->setModele(fiches);
+    fragmentFiches->getTableau()->resizeColumnsToContents();
+    relacherFiche();
 }
 
-void ControleurClients::afficherFiches(QModelIndex index)
-{
+QString *ControleurClients::requeteFiches(int idAppareil) const {
+    return new QString("SELECT * FROM fiches WHERE idAppareil="+QString::number(idAppareil));
+}
+
+void ControleurClients::selectionnerClient(QModelIndex caseSelectionnee) {
+    fragmentClients->getBouton2()->setEnabled(true);
+    fragmentClients->getBouton3()->setEnabled(true);
     int colonne = 0;
-    int rangee = index.row();
-    QModelIndex index2 = fragmentAppareils->getTableau()->model()->index(rangee, colonne);
-    QVariant id = fragmentAppareils->getTableau()->model()->data(index2);
-    peuplerFiches(id.toInt());
-    fragmentFiches->show();
+    int rangee = caseSelectionnee.row();
+    QModelIndex caseId = fragmentClients->getTableau()->model()->index(rangee, colonne);
+    int id = fragmentClients->getTableau()->model()->data(caseId).toInt();
+    emit clientSelectionne(id);
 }
 
+void ControleurClients::relacherClient() {
+    fragmentClients->getTableau()->clearSelection();
+    fragmentClients->getBouton2()->setEnabled(false);
+    fragmentClients->getBouton3()->setEnabled(false);
+    emit clientRelache();
+}
+
+void ControleurClients::selectionnerAppareil(QModelIndex caseSelectionnee) {
+    fragmentAppareils->getBouton2()->setEnabled(true);
+    fragmentAppareils->getBouton3()->setEnabled(true);
+    int colonne = 0;
+    int rangee = caseSelectionnee.row();
+    QModelIndex caseId = fragmentAppareils->getTableau()->model()->index(rangee, colonne);
+    int id = fragmentAppareils->getTableau()->model()->data(caseId).toInt();
+    emit appareilSelectionne(id);
+}
+
+void ControleurClients::relacherAppareil() {
+    fragmentAppareils->getTableau()->clearSelection();
+    fragmentAppareils->getBouton2()->setEnabled(false);
+    fragmentAppareils->getBouton3()->setEnabled(false);
+    emit appareilRelache();
+}
+
+void ControleurClients::selectionnerFiche(QModelIndex caseSelectionnee) {
+    fragmentFiches->getBouton2()->setEnabled(true);
+    fragmentFiches->getBouton3()->setEnabled(true);
+    int colonne = 0;
+    int rangee = caseSelectionnee.row();
+    QModelIndex caseId = fragmentAppareils->getTableau()->model()->index(rangee, colonne);
+    int id = fragmentFiches->getTableau()->model()->data(caseId).toInt();
+    emit ficheSelectionnee(id);
+}
+
+void ControleurClients::relacherFiche() {
+    fragmentFiches->getTableau()->clearSelection();
+    fragmentFiches->getBouton2()->setEnabled(false);
+    fragmentFiches->getBouton3()->setEnabled(false);
+    emit ficheRelachee();
+}
--- InfoDynamiqueQt/Controleurs/controleurclients.h
+++ InfoDynamiqueQt/Controleurs/controleurclients.h
@@ -39,11 +39,20 @@ private:
     // Le fragment des clients
     VueFragment *fragmentClients;
 
+    // Le numéro de la colonne contenant les id des clients dans fragmentClients
+    int colonneIdClients;
+
     // Le fragment des appareils
     VueFragment *fragmentAppareils;
 
+    // Le numéro de la colonne contenant les id des appareils dans fragmentAppareils
+    int colonneIdAppareils;
+
     // Le fragment des fiches
     VueFragment *fragmentFiches;
+
+    // Le numéro de la colonne contenant les id des fiches dans fragmentFiches
+    int colonneIdFiches;
 
     /**
      * @brief configurerFragmentClients
@@ -63,13 +72,135 @@ private:
      */
     void configurerFragmentFiches();
 
+    /**
+     * @brief requeteClients
+     * La requête utilisée pour peupler les clients
+     */
+    const QString* requeteClients;
+
+    /**
+     * @brief requeteAppareils
+     * @param idClient Le client dont on souhaite afficher les appareils
+     * @return La requête utilisée pour peupler les appareils
+     */
+    QString* requeteAppareils(int idClient) const;
+
+    /**
+     * @brief requeteFiches
+     * @param idAppareil L'appareil dont on souhaite afficher les fiches
+     * @return La requête utilisée pour peupler les fiches
+     */
+    QString* requeteFiches(int idAppareil) const;
+
 signals:
 
+    /**
+     * @brief clientSelectionne
+     * Signal émis lorsqu'un client est sélectionné dans fragmentClients
+     * @param int L'id du client
+     */
+    void clientSelectionne(int);
+
+    /**
+     * @brief clientRelache
+     * Signal émis lorsque le client sélectionné est relâché
+     */
+    void clientRelache();
+
+    /**
+     * @brief appareilSelectionne
+     * Signal émis lorsqu'un appareil est sélectionné dans fragmentAppareils
+     * @param int L'id de l'appareil
+     */
+    void appareilSelectionne(int);
+
+    /**
+     * @brief appareilRelache
+     * Signal émis lorsque l'appareil sélectionné est relâché
+     */
+    void appareilRelache();
+
+    /**
+     * @brief ficheSelectionnee
+     * Signal émis lorsqu'une fiche est sélectionnée dans fragmentFiche
+     */
+    void ficheSelectionnee(int);
+
+    /**
+     * @brief ficheRelachee
+     * Signal émis lorsque la fiche sélectionnée est relâchée
+     * @param int L'id de la fiche
+     */
+    void ficheRelachee();
+
 public slots:
-    void clientSelectionne(QModelIndex);
-    void afficherFiches(QModelIndex);
+
+    /**
+     * @brief selectionnerClient
+     * Réagit à une sélection de client
+     * Émet clientSelectionne
+     * @param QModelIndex L'index de la case sélectionnée dans fragmentClients
+     */
+    void selectionnerClient(QModelIndex);
+
+    /**
+     * @brief relacherClient
+     * Réagit à une désélection de client
+     * Émet clientRelache
+     */
+    void relacherClient();
+
+    /**
+     * @brief peuplerClients
+     * Peuple la table du fragmentClients avec tous les clients
+     * Émet clientRelache
+     */
     void peuplerClients();
+
+    /**
+     * @brief selectionnerAppareil
+     * Réagit à une sélection d'appareil
+     * Émet appareilSelectionne
+     * @param QModelIndex L'index de la case sélectionnée dans fragmentAppareils
+     */
+    void selectionnerAppareil(QModelIndex);
+
+    /**
+     * @brief relacherAppareil
+     * Réagit à une désélection d'appareil
+     * Émet appareilRelache
+     */
+    void relacherAppareil();
+
+    /**
+     * @brief peuplerAppareils
+     * Peuple la table de fragmentAppareils avec tous les appareils du client donné
+     * Émet appareilRelache
+     * @param int L'id du client
+     */
     void peuplerAppareils(int idClient);
+
+    /**
+     * @brief selectionnerFiche
+     * Réagit à une sélection de fiche
+     * Émet ficheSelectionnee
+     * @param QModelIndex L'index de la case sélectionnée dans fragmentFiches
+     */
+    void selectionnerFiche(QModelIndex);
+
+    /**
+     * @brief relacherFiche
+     * Réagit à une désélection de fiche
+     * Émet ficheRelachee
+     */
+    void relacherFiche();
+
+    /**
+     * @brief peuplerFiches
+     * Peuple la table de fragmentFiches avec toutes les fiches de l'appareil donné
+     * Émet ficheRelachee
+     * @param int L'id de l'appareil
+     */
     void peuplerFiches(int idAppareil);
 };
 
