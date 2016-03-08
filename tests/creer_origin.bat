e213888e93d81d3e7d0ed9e1afb27582e70d8a56
jerped <jerm95@hotmail.com>
2016-03-08 17:18:43 -0500

    ajout des tests pour les fiches et la priorite.

--- tests/creer_origin.bat
+++ tests/creer_origin.bat
@@ -1,4 +1,7 @@
 echo off
 "C:\Program Files (x86)\MySQL\MySQL Server 5.6\bin\mysql.exe" "--defaults-file=C:\ProgramData\MySQL\MySQL Server 5.6\my.ini" "-uroot" "-ppatate" < restart.sql
-c:\Python27\python.exe creer_client.py origin
+c:\Python27\python.exe creer_fiche_priorite1.py origin
+c:\Python27\python.exe creer_fiche_priorite2.py origin
+c:\Python27\python.exe creer_fiche_priorite3.py origin
+c:\Python27\python.exe creer_fiche_priorite4.py origin
 pause
